4a5fa19e821de737011ca370eaa292e7d1dea255
s12f <97083380+s12f@users.noreply.github.com>
2023-05-06 15:51:14 +0800

    add get subscription tests (#98)

--- app/src/test/java/io/hstream/testing/Subscription.java
+++ app/src/test/java/io/hstream/testing/Subscription.java
@@ -5,6 +5,7 @@ import static io.hstream.testing.TestUtils.*;
 import io.hstream.HStreamClient;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Tag;
@@ -38,6 +39,34 @@ public class Subscription {
             .sorted()
             .collect(Collectors.toList());
     Assertions.assertEquals(subscriptions.stream().sorted().collect(Collectors.toList()), res);
+  }
+
+  @Test
+  @Timeout(60)
+  void testGetSubscription() {
+    String streamName = randStream(client);
+    String subName = randSubscription(client, streamName);
+    var sub = client.getSubscription(subName);
+    Assertions.assertEquals(subName, sub.getSubscription().getSubscriptionId());
+    Assertions.assertEquals(streamName, sub.getSubscription().getStreamName());
+    Assertions.assertNotNull(sub.getOffsets());
+  }
+
+  @Test
+  @Timeout(60)
+  void testGetSubscriptionWithConsumer() throws Exception {
+    String streamName = randStream(client);
+    String subName = randSubscription(client, streamName);
+    var producer = client.newBufferedProducer().stream(streamName).build();
+    // write and consume some data
+    TestUtils.produce(producer, 100, 10);
+    var count = new AtomicInteger();
+    TestUtils.consume(client, subName, 10, receivedRawRecord -> count.incrementAndGet() < 10);
+
+    var sub = client.getSubscription(subName);
+    Assertions.assertEquals(subName, sub.getSubscription().getSubscriptionId());
+    Assertions.assertEquals(streamName, sub.getSubscription().getStreamName());
+    Assertions.assertFalse(sub.getOffsets().isEmpty());
   }
 
   @Test
