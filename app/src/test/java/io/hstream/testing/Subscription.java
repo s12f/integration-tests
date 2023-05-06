785a2f6242ad9756f448e9a795e3b95093664db1
s12f <siwen.feng@emqx.io>
2023-05-06 15:26:39 +0800

    fix

--- app/src/test/java/io/hstream/testing/Subscription.java
+++ app/src/test/java/io/hstream/testing/Subscription.java
@@ -15,8 +15,6 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import javax.swing.text.Utilities;
-
 @Tag("basicTest")
 @ExtendWith(ClusterExtension.class)
 public class Subscription {
